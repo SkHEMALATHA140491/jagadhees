a073c8be35df835d0619373785f43cf122d45dfa
jagadhees <jagadhees3@gmail.com>
2016-07-16 13:21:51 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Student
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int i=0;i<5;i++){
+       
+       
+           System.out.println("Hello");
+       
+       }
+    }
+}
