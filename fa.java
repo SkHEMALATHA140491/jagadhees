e39846394fdd87bf48751fa6ff5c3f7eb29d45ff
jagadhees <jagadhees3@gmail.com>
2016-07-16 13:09:40 +0530

    Add files via upload

---
+++ fa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hi;
+
+import java.util.Scanner;
+
+
+public class Hi {
+
+   
+    public static void main(String[] args) {
+               Scanner scanner = new Scanner(System.in);
+       System.out.print("Enter the number whose factorial is to be found: ");
+       int n = scanner.nextInt();
+       int result = factorial(n);
+       System.out.println("The factorial of " + n + " is " + result);
+   }
+
+   public static int factorial(int n) {
+       int result = 1;
+       for (int i = 1; i <= n; i++) {
+           result = result * i;
+       }
+       return result;
+   }
+}
+    
+
