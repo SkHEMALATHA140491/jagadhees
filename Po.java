269b9261b522ef6afd5df084dacebdb115a5815a
jagadhees <jagadhees3@gmail.com>
2016-07-18 11:51:25 +0530

    Add files via upload

---
+++ Po.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package po;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Po {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter  the no:");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("The given number is Possitive");
+        }
+        else
+        {
+            System.out.println("The given number is negative");
+        }
+        }
+            
+}
