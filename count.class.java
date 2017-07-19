da1c4996139adb65a74c472bf7f76fa98bd17d37
SkHEMALATHA140491 <hemalatha571997@gmail.com>
2017-07-19 14:11:59 +0530

    Create count.class.java

---
+++ count.class.java
@@ -0,0 +1,13 @@
+package latha;
+public class countallsubstrings{
+public static void main(String arga[]){
+String s="hemalatha";
+for(int i=0;i<s.length();i++){
+for(int j=1;j<s1.length()-i;j++){
+System.out.println(s1.substring(i,i+j+1));
+	count++;
+  }
+  }
+  System.out.println(count);
+  }
+  }
